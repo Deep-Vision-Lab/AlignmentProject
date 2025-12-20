423d8ca921951acfa860399a9c5ef2c5d68f6dfe
mas98ahmed <ahmedmas@post.bgu.ac.il>
2025-12-20 02:20:26 +0200

    update

--- Parameters.py
+++ Parameters.py
@@ -9,7 +9,7 @@ epochs = 300
 learning_rate = 1e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 debug = True # Set to True to save patches and heatmaps for debugging
-debug_wandb = False # Set to True to log training to Weights & Biases
+debug_wandb = True # Set to True to log training to Weights & Biases
 show_gradients = True # Set to True to print gradients for debugging
 
 matchScore = 10
