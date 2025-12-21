25653da92b9c110455732264257e0bf9c2687e0d
Ahmed Massalha <ahmedmas@post.bgu.ac.il>
2025-12-22 01:09:41 +0200

    clean code

--- wandb_config.py
+++ wandb_config.py
@@ -5,7 +5,7 @@ def init_wandb():
     wandb.init(
             # set the wandb project where this run will be logged
             project="AlignmentProject",
-            name=f"Train model {window_size} - {model_arch} - {loss_type} - {normalize_type} - AMP",
+            name=f"Train model {window_size} - {model_arch} - {loss_type} - {normalize_type}",
             # track hyperparameters and run metadata
             config={
                 "learning_rate": learning_rate,
@@ -17,3 +17,11 @@ def init_wandb():
                 "slicing_window_width": window_size,
                 "normalizing method ": normalize_type
             })
+    
+def update_wandb(train_loss, val_loss, train_accuracy, val_accuracy):
+    wandb.log({
+    "train_loss": train_loss, 
+    "val_loss": val_loss,
+    "train_accuracy": train_accuracy,
+    "val_accuracy": val_accuracy
+    })
